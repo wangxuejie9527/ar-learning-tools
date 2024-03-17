68fecc57531644babec73cec8b9ed8a05a4e3290
wangxuejie <1353033720@qq.com>
2024-03-17 16:00:49 +0800

    v2

--- miniprogram/utils/util.ts
+++ miniprogram/utils/util.ts
@@ -13,6 +13,17 @@ export const formatTime = (date: Date) => {
   )
 }
 
+export const formatTimeYYMMDD = (date: Date) => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  return (
+    // [year, month, day].map(formatNumber).join('-')
+    '2024-03-15'
+  )
+}
+
 const formatNumber = (n: number) => {
   const s = n.toString()
   return s[1] ? s : '0' + s

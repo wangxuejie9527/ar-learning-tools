1d466c3af61362da58835f0a3942ac102e3697df
JOJO <jojo@MacBook-Pro.local>
2024-03-17 15:34:40 +0800

    Revert "ut"
    This reverts commit 1068553bbf7b0aee02933ac36879f5caa6bdbe03.

--- miniprogram/utils/util.ts
+++ miniprogram/utils/util.ts
@@ -13,16 +13,6 @@ export const formatTime = (date: Date) => {
   )
 }
 
-export const formatTimeYYMMDD = (date: Date) => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-
-  return (
-    [year, month, day].map(formatNumber).join('-')
-  )
-}
-
 const formatNumber = (n: number) => {
   const s = n.toString()
   return s[1] ? s : '0' + s
